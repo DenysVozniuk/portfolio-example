5209b61b2cea4b44d7ff67946d306e075af05e82
Denys <denvoznyuk12@gmail.com>
2024-10-16 20:19:51 +0300

    mobile menu open changes 1.0

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const mobileBtn = document.querySelector('.burger-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const body = document.querySelector('body');
 
 const toggleMenu = () => {
     mobileBtn.classList.toggle('active');
     mobileMenu.classList.contains('open') ? (
         mobileMenu.classList.remove('open'),
-        mobileMenu.classList.add('close')
+        mobileMenu.classList.add('close'),
+        body.removeAttribute('style')
     ) : (
+        body.setAttribute('style', 'overflow: hidden'),
         mobileMenu.classList.remove('close'),
         mobileMenu.classList.add('open')
     );
